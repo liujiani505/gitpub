19dbe316c0b2d41305cd437879aa90aa2473f956
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2021-09-15 13:27:15 -0400

    finished the food portion

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const drinks = require("./models/drinks");
+const foods = require("./models/foods");
 
 app.get("/", (req, res)=>{
     res.send("Welcome to the Github App!")
@@ -13,6 +14,14 @@ app.get("/drinks", (req, res)=>{
 
 app.get("/drinks/:id", (req, res)=>{
     res.render("drinks_show.ejs", {drink: drinks[req.params.id]})
+})
+
+app.get("/foods", (req, res)=>{
+    res.render("foods_index.ejs", {allFood: foods })
+})
+
+app.get("/foods/:id", (req, res)=>{
+    res.render("foods_show.ejs", {food: foods[req.params.id] })
 })
 
 app.listen(port, ()=>{
